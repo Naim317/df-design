5f7daaa329eb7038392c505585a3b349dc97ba8a
Md. Jannat-UL Naim <72838150+Naim317@users.noreply.github.com>
2023-07-08 20:17:27 +0600

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-    if ($(window).width() > 991) {
-        $('.navbar-light .d-menu').hover(function() {
+$(document).ready(function () {
+    if ($(window).width() > 991){
+    $('.navbar-light .d-menu').hover(function () {
             $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-        }, function() {
+        }, function () {
             $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
         });
-    }
-});
+        }
+    });
+
+    var carousel = document.getElementById('bannerCarousel');
+    var bsCarousel = new bootstrap.Carousel(carousel, {
+    interval: 5000, 
+    pause: false, 
+    ride: 'carousel' 
+  });
