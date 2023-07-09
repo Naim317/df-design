38b0b866c836cae320823ed8fd5bc177b9ea2e99
Md. Jannat-UL Naim <72838150+Naim317@users.noreply.github.com>
2023-07-09 21:32:13 +0600

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,47 @@
-$(document).ready(function () {
-    if ($(window).width() > 991){
-    $('.navbar-light .d-menu').hover(function () {
-            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-        }, function () {
-            $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
-        });
-        }
-    });
+// ForBannerSlider
+var carousel = document.getElementById('bannerCarousel');
+var bsCarousel = new bootstrap.Carousel(carousel, {
+interval: 5000, 
+pause: false, 
+ride: 'carousel' 
+});
 
-    var carousel = document.getElementById('bannerCarousel');
-    var bsCarousel = new bootstrap.Carousel(carousel, {
-    interval: 5000, 
-    pause: false, 
-    ride: 'carousel' 
-  });
+// ForTestimonialSlider
+$(document).ready(function () {
+    var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+  );
+  if (window.matchMedia("(min-width: 576px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+      wrap: false
+    });
+    var carouselWidth = $("#carouselExampleControls .carousel-inner")[0].scrollWidth;
+    var cardWidth = $("#carouselExampleControls .carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselExampleControls .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 3) {
+        scrollPosition += cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 1) {
+        scrollPosition -= cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+});
+
+
+
+
